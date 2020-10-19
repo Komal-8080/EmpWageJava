db2d8b83b078f1d9aebb95be2530e6742fbb8909
Komal-8080 <shindeg163@gmail.com>
2020-10-19 13:55:35 +0530

    EmpCheck Done UC1

--- EmpWagejava.java
+++ EmpWagejava.java
@@ -5,5 +5,16 @@ public class EmpWagejava
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		System.out.println("Welcome to Employee wage computation program");
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		int empCheck = (int)(Math.random() * 2);
+		int Full_Time = 1;
+
+	    if ( empCheck == 1 )
+	    {
+		System.out.println("Employee Present");
+	    }
+	    else
+	    {
+		System.out.println("Employee Absent");
+	    }
 	}
 }
