68fd513567959af0e65327ec43fb88aab2ab2d68
Komal-8080 <shindeg163@gmail.com>
2020-10-19 14:06:03 +0530

    Daily Wages calculated UC2

--- EmpWagejava.java
+++ EmpWagejava.java
@@ -7,14 +7,19 @@ public class EmpWagejava
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		int empCheck = (int)(Math.random() * 2);
 		int Full_Time = 1;
+		int Wage_per_hour = 20;
+		int WorkingHours = 0;
 
 	    if ( empCheck == 1 )
 	    {
 		System.out.println("Employee Present");
+		WorkingHours = 8;
 	    }
 	    else
 	    {
 		System.out.println("Employee Absent");
 	    }
+	int dailyEmpWage = WorkingHours * Wage_per_hour;
+		System.out.println("Daily Employee wage is " +dailyEmpWage);
 	}
 }
