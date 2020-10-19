2f924e33bc02f2feb4cd768d05eb1fd522a91d6c
Komal-8080 <shindeg163@gmail.com>
2020-10-19 16:11:25 +0530

    Switch case used UC4

--- EmpWagejava.java
+++ EmpWagejava.java
@@ -1,28 +1,28 @@
 public class EmpWagejava
 {
+		public static final int Full_Time = 2;
+                public static final int Part_Time = 1;
+                public static final int Wage_per_hour = 20;
+
 	public static void main(String[] args)
 	{
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		System.out.println("Welcome to Employee wage computation program");
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		int empCheck = (int)(Math.random() * 3);
-		int Full_Time = 2;
-		int Part_Time = 1;
-		int Wage_per_hour = 20;
 		int WorkingHours = 0;
 
-	    if ( empCheck == 2 )
+	    switch  (empCheck)
 	    {
+	    case Full_Time:
 		System.out.println("Employee Present");
 		WorkingHours = 8;
-	    }
-	    else if ( empCheck == 1 )
-	    {
+	    break;
+	    case Part_Time:
 		System.out.println("Employee Present");
 		WorkingHours = 4;
-	    }
-	    else
-	    {
+	    break;
+	    default:
 		System.out.println("Employee Absent");
 	    }
 	int dailyEmpWage = WorkingHours * Wage_per_hour;
