630f7635dabfe40e4917ae8cd8287076bf9fac75
Komal-8080 <shindeg163@gmail.com>
2020-10-19 14:11:04 +0530

    Part Time Employee added UC3

--- EmpWagejava.java
+++ EmpWagejava.java
@@ -5,15 +5,21 @@ public class EmpWagejava
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		System.out.println("Welcome to Employee wage computation program");
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-		int empCheck = (int)(Math.random() * 2);
-		int Full_Time = 1;
+		int empCheck = (int)(Math.random() * 3);
+		int Full_Time = 2;
+		int Part_Time = 1;
 		int Wage_per_hour = 20;
 		int WorkingHours = 0;
 
-	    if ( empCheck == 1 )
+	    if ( empCheck == 2 )
 	    {
 		System.out.println("Employee Present");
 		WorkingHours = 8;
+	    }
+	    else if ( empCheck == 1 )
+	    {
+		System.out.println("Employee Present");
+		WorkingHours = 4;
 	    }
 	    else
 	    {
