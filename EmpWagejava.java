6503221eb17a3edae9ff728e860da0390b4afb97
Komal-8080 <shindeg163@gmail.com>
2020-10-19 13:46:36 +0530

    Welcome message

---
+++ EmpWagejava.java
@@ -0,0 +1,9 @@
+public class EmpWagejava
+{
+	public static void main(String[] args)
+	{
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("Welcome to Employee wage computation program");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	}
+}
